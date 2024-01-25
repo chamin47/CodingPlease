639880e004f20c3d1b204385a5325a782592d3af
chamin47 <152050575+chamin47@users.noreply.github.com>
2024-01-25 12:56:36 +0900

    [Feat]: 싱글턴 구현
    추후에 많이 쓰일 거 같아 미리 싱글턴 패턴 구현했습니다.

---
+++ Assets/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using System.Net.Sockets;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public static GameManager instance = null;
+
+	private void Awake()
+	{
+		// instance가 할당되지 않았을 경우
+		if (instance == null)
+		{
+			instance = this;
+		}
+		// instance에 할당된 클래스의 인스턴스가 다를 경우 새로 생성된 클래스를 의미함
+		else if (instance != this)
+		{
+			Destroy(gameObject);
+		}
+
+		// 다른 씬으로 넘어가더라도 삭제하지 않고 유지함
+		DontDestroyOnLoad(this.gameObject);
+	}
+}
