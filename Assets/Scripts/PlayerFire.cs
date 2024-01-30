f7e62f083db9fda1e6e1e69a1f54f5a5d51447ee
chamin47 <152050575+chamin47@users.noreply.github.com>
2024-01-31 07:55:58 +0900

    [fix] 헬프 아이템 사용 시 화면 전체 반짝임 기능 추가
    천진반의 태양권이 생각나서 한 번 구현해 봤습니다!

--- Assets/Scripts/PlayerFire.cs
+++ Assets/Scripts/PlayerFire.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerFire : MonoBehaviour
 {  
@@ -11,6 +12,8 @@ public class PlayerFire : MonoBehaviour
 	public AudioClip clip2;
     public GameObject bulletPrefab;
     public GameObject firePosition;
+	public Image flashImage;
+	public float flashDuration = 0.1f;
 
     void FixedUpdate()
     {
@@ -35,6 +38,7 @@ public class PlayerFire : MonoBehaviour
 		if (Input.GetKeyDown(KeyCode.LeftAlt) && GameManager.instance.clearAbilityCount > 0)
 		{
 			ClearEnemiesAndBullets();
+			FlashScreenEffect();
 			GameManager.instance.UseClearAbility(); // 사용 횟수 감소
 		}
 	}
@@ -57,5 +61,25 @@ public class PlayerFire : MonoBehaviour
 			Destroy(bullet);
 		}
 		SoundManager.instance.SFXPlay2("Bomb", clip2);
+	}
+
+	private void FlashScreenEffect()
+	{
+		StartCoroutine(DoFlash());
+	}
+
+	private IEnumerator DoFlash()
+	{
+		float time = 0;
+
+		while (time < flashDuration)
+		{
+			time += Time.deltaTime;
+			float alpha = Mathf.Sin(time / flashDuration * Mathf.PI);
+			flashImage.color = new Color(1, 1, 1, alpha);
+			yield return null;
+		}
+
+		flashImage.color = new Color(1, 1, 1, 0); // 번쩍임 종료 후 이미지를 다시 투명하게 만듦
 	}
 }
